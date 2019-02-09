ae08e54609ecdfc0d4980dea46474c5a40c19b78
Andrew Dacunto <43014649+andrawhoodoo@users.noreply.github.com>
2019-02-08 20:00:46 -0500

    imported db collections
    fixed imports via server/main.js

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+
+// Must import all desired db collections as such
 import '../imports/api/surveys';
+import '../imports/api/questions';
+import '../imports/api/users';
 
 Meteor.startup(() => {
   // code to run on server at startup

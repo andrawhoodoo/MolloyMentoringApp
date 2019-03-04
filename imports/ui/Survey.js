9a0f1f9446033eb6f06b8b7e46cd7d72593ba5e4
andrawhoodoo <adacunto@lions.molloy.edu>
2019-03-03 22:55:43 -0500

    Update Survey.js

--- imports/ui/Survey.js
+++ imports/ui/Survey.js
@@ -3,6 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Surveys } from '../api/surveys';
+import { Questions } from '../api/questions';
+import { Options } from '../api/options';
+import { Answers } from '../api/answers';
 
 export default class Survey extends React.Component {
 	constructor(props) {
@@ -22,25 +25,38 @@ export default class Survey extends React.Component {
 		this.surveyTracker.stop();
 	}
 	renderQuestions() {
-		return(
-			<section>
-				<h3> Mentoring App Survey </h3>
-				<form>
-					<p> 1.  What is your Gender? </p>
+		return this.state.survey.questions.map(questionId => {
+			Meteor.subscribe('questionsData');
+			const myQuestion = Questions.findOne({_id: questionId});
+			return (
+				<div key={questionId}>
+					<p>{myQuestion.text}</p>
 					<ul>
-					  <li><input type = "radio" name = "gender" value = "male" /></li>
-					  <li><input type = "radio" name = "gender" value = "male" /></li>
-					  <li><input type = "radio" name = "gender" value = "male" /></li>
-					  <li><input type = "radio" name = "gender" value = "male" /></li>
+					{this.renderOptions(myQuestion)}
 					</ul>
-				</form>
-			</section>
-		);
+				</div>
+			);
+		});
+	}
+	renderOptions(question) {
+		return question.options.map(optionId => {
+			Meteor.subscribe('optionsData');
+			const myOption = Options.findOne({_id: optionId});
+			return (
+				<li><input type ="radio" value={optionId} />{myOption.text}</li>
+			);
+		});
 	}
 	render() {
 		return (
 			<div>
-				{this.renderQuestions()}
+				<section>
+					<h3> Mentoring App Survey </h3>
+					<form>
+						{this.renderQuestions()}
+						<button className='btn btn-danger text-white' onClick={this.submitSurvey.bind(this)}>Submit Your Answers</button>
+					</form>
+				</section>
 			</div>
 		);
 	}

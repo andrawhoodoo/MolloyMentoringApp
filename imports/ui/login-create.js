5f22b4f3f87d582a8b1fb751b929cb18fa42d30c
msaggio <34450701+msaggio@users.noreply.github.com>
2019-03-06 16:32:07 -0500

    login-create styling

--- imports/ui/login-create.js
+++ imports/ui/login-create.js
@@ -39,32 +39,39 @@ export default class CreateUser extends React.Component {
 
     render() {
         return(
-            <div id="Login-Box">
-                <div id="Login-Logo-Float-Left">
-                    <img id="Login-Logo" src="Online-Mentoring-Login-Logo-2.png" />
-                    <h1> Molloy College Online Mentoring App </h1>
+            <div id="Login-Box" className="bg-danger text-white">
+                <div id="Login-Logo-Float-Left" className="text-center mb-5">
+                    <h1 className='bg-dark py-3'> Molloy College Online Mentoring App </h1>
                 </div>
-                <div id="Login-Info-Float-Right">
-                    <div id="Login-Section">
-                        <img className="Molloy-Logo" src="Molloy-Logo-Real.png" />
-                        <h1> Create Account </h1>
-                        <form onSubmit={this.onSubmit.bind(this)} noValidate>
-                            <p>First Name: </p>
-                            <input className= "Login-Box" type="text" ref="firstName" name="firstName" placeholder="Enter First Name" />
+                <div id="Login-Info-Float-Right" className='container pb-5'>
+                    <div id="Login-Section" className='card bg-dark pb-5'>
+                        <div className="card-head">
+                        	<h1> Create Account </h1>
+                        </div>
+                        <div className="card-body">
+	                        <form className="mb-3" onSubmit={this.onSubmit.bind(this)} noValidate>
 
-                            <p>Last Name: </p>
-                            <input className="Login-Box" type="text" ref="lastName" name="lastName" placeholder="Enter Last Name" />
-
-                            <p>Email: </p>
-                            <input className="Login-Box" type="Email" ref="email" name="emailAddress" placeholder="Enter Email Address" />
-
-                            <p>Password: </p>
-                            <input className="Login-Box" type="password" ref="password" name="password" placeholder="Enter Password" />
-
-                            <input type="submit" name="" value="Create" />
-                        </form>
-						{this.state.error ? <p className="text-warning">{this.state.error}</p> : undefined}
-						<Link to="/">Already have an account?</Link>
+	                        	<div className="mb-3" >
+										<h4>First Name: </h4>
+										<input className= "Login-Box" type="firstName" ref='firstName' name="firstName" placeholder="Enter First Name" />
+									</div>
+									<div className="mb-3">
+										<h4>Last Name: </h4>
+										<input className="Login-Box" type="lastName" ref='lastName' name="lastName" placeholder="Enter Last Name" />
+									</div>
+									<div className="mb-3" >
+										<h4>Email: </h4>
+										<input className= "Login-Box" type="email" ref='email' name="email" placeholder="Enter email" />
+									</div>
+									<div className="mb-3">
+										<h4>Password: </h4>
+										<input className="Login-Box" type="password" ref='password' name="password" placeholder="Enter Password" />
+									</div>
+	                            <input className="btn btn-danger text-white" type="submit" name="" value="Create" />
+	                        </form>
+							{this.state.error ? <p className="text-warning">{this.state.error}</p> : undefined}
+							<Link className="btn btn-primary text-white" to="/">Already have an account?</Link>
+                    	</div>
                     </div>
                 </div>
             </div>
